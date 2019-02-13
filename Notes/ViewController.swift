0d94648b78576a808ccd87dae4e07de3051effa8
Ben Rovik <benrovik@root1s-macbook-pro-2.local>
2019-02-12 18:37:47 -0600

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Ben Rovik on 2/12/19.
+//  Copyright © 2019 Ben Rovik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
